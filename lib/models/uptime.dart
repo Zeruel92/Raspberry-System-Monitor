143c80182cd73b2d9de22b155814a7ff422da03d
Gabriele Tramonte <gabry.t92@gmail.com>
2019-03-13 20:39:11 +0100

    fix import

--- lib/models/uptime.dart
+++ lib/models/uptime.dart
@@ -5,7 +5,7 @@ import 'dart:convert';
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
-
+import 'serializers.dart';
 part 'uptime.g.dart';
 
 abstract class Uptime implements Built<Uptime, UptimeBuilder> {
