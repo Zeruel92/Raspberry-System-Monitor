4e0c77015a7a8000acffe7c1baabce50ad393250
Gabriele Tramonte <gabry.t92@gmail.com>
2019-06-13 21:54:07 +0200

    recalculating flexes values

--- lib/main.dart
+++ lib/main.dart
@@ -46,20 +46,14 @@ class _State extends State<MyApp> {
       body: Center(
         child: Column(
           children: <Widget>[
-            Flexible(child: AddressTile(), flex: 2),
-            Flexible(child: LoadAvg(), flex: 7),
-            Flexible(child: DiskTile(), flex: 6,),
-            Flexible(
-              child: TorrentTile(),
-              flex: 9,
-            ),
-            Flexible(
-              child: TeledartTile(),
-              flex: 4,
-            ),
-            Flexible(child: SambaTile(), flex: 4),
-            Flexible(child: SSHTile(), flex: 4),
-            Flexible(child: NetatalkTile(), flex: 4)
+            Flexible(child: AddressTile(), flex: 1),
+            Flexible(child: LoadAvg(), flex: 4),
+            Flexible(child: DiskTile(), flex: 4,),
+            Flexible(child: TorrentTile(), flex: 4),
+            Flexible(child: TeledartTile(), flex: 2),
+            Flexible(child: SambaTile(), flex: 2),
+            Flexible(child: SSHTile(), flex: 2),
+            Flexible(child: NetatalkTile(), flex: 2)
           ],
         ),
       ),
