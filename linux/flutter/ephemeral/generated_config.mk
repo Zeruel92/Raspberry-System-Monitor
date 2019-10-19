0acd0247f38f1f0e0f3eae6fc87fa1d6552865e3
Gabriele Tramonte <gabry.t92@gmail.com>
2019-10-19 14:36:53 +0200

    new linux embedder

---
+++ linux/flutter/ephemeral/generated_config.mk
@@ -0,0 +1,5 @@
+# Generated code do not commit.
+export FLUTTER_ROOT=/home/gabry/SSD/IDE/flutter
+export TRACK_WIDGET_CREATION=false
+export FLUTTER_TARGET=/home/gabry/FlutterProjects/rpi/raspberry_system_monitor/lib/main.dart
+export PROJECT_DIR=/home/gabry/FlutterProjects/rpi/raspberry_system_monitor
