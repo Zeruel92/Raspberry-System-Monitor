9004ef41a613942f16fd12e11550cc639a459e74
Gabriele Tramonte <gabry.t92@gmail.com>
2020-06-24 13:37:17 +0200

    view folder with ui files

---
+++ lib/views/landing.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Landing extends StatefulWidget {
+  @override
+  _LandingState createState() => _LandingState();
+}
+
+class _LandingState extends State<Landing> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
---
+++ lib/views/main.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart'
+    show debugDefaultTargetPlatformOverride;
+import 'package:flutter/material.dart';
+import 'package:raspberry_system_monitor/blocs/bloc.dart';
+
+import 'home.dart';
+import 'landing.dart';
+
+void main() {
+  _setTargetPlatformForDesktop();
+  runApp(MyApp());
+}
+
+void _setTargetPlatformForDesktop() {
+  TargetPlatform targetPlatform;
+  if (Platform.isMacOS) {
+    targetPlatform = TargetPlatform.iOS;
+  } else if (Platform.isLinux || Platform.isWindows) {
+    targetPlatform = TargetPlatform.android;
+  }
+  if (targetPlatform != null) {
+    debugDefaultTargetPlatformOverride = targetPlatform;
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+}
+
+class _State extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<bool>(
+        stream: Bloc.instance.theme.stream,
+        builder: (context, snapshot) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: Platform.isLinux ? false : true,
+            title: 'Raspberry System Monitor',
+            theme: snapshot.data ? ThemeData.dark() : ThemeData.light(),
+            darkTheme: ThemeData(
+              brightness: Brightness.dark,
+            ),
+            initialRoute: '/',
+            routes: {
+              '/': (context) => Home(),
+              '/landing': (context) => Landing()
+            },
+          );
+        });
+  }
+
+  @override
+  void dispose() {
+    Bloc.instance.close();
+    super.dispose();
+  }
+}
