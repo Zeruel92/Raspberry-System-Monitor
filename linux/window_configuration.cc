c1be0340740f23698397dc420e4c66776b118ecc
Gabriele Tramonte <gabry.t92@gmail.com>
2020-06-24 18:52:10 +0200

    new linux embedder files

--- linux/window_configuration.cc
+++
@@ -1,5 +0,0 @@
-#include "window_configuration.h"
-
-const char *kFlutterWindowTitle = "raspberry_system_monitor";
-const unsigned int kFlutterWindowWidth = 800;
-const unsigned int kFlutterWindowHeight = 600;
