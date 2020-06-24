a033ee58dc94691be006a3319e13598a1d1aaa8f
Gabriele Tramonte <gabry.t92@gmail.com>
2020-06-24 18:58:06 +0200

    updated snapcraft.yaml

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -6,7 +6,7 @@
 import 'dart:ui';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:shared_preferences_web/shared_preferences_webdart';
 
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
